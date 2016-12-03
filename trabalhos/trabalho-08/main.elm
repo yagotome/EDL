c071489f645d2cc6db1f90fbb07ac49333333e39
Yago Tomé <yago.tome@hotmail.com>
2016-12-03 17:50:39 -0200

    Criando operações aritiméticas
    (Adição já estava feita)
    Subtração
    Multiplicação
    Divisão
    Módulo
    Raiz enésima piso
    Raiz enésima teto
    Raiz enésima arredondada
    Exponencial
    Logarítmo piso
    Logarítmo teto
    Logarítmo arredondado

--- trabalhos/trabalho-08/main.elm
+++ trabalhos/trabalho-08/main.elm
@@ -5,6 +5,17 @@ zero : Env
 zero = \ask -> 0
 
 type Exp =  Add Exp Exp
+            | Sub Exp Exp
+            | Times Exp Exp
+            | Div Exp Exp
+            | Mod Exp Exp
+            | Exponential Exp Exp
+            | NthRootf Exp Exp
+            | NthRootc Exp Exp
+            | NthRootr Exp Exp
+            | Logf Exp Exp
+            | Logc Exp Exp
+            | Logr Exp Exp
             | Num Int
             | Var String
 
@@ -17,9 +28,20 @@ e1 = Add (Num 9) (Num 1)
 evalExp : Exp -> Env -> Int
 evalExp exp env =
     case exp of
-        Add exp1 exp2  -> (evalExp exp1 env) + (evalExp exp2 env)
-        Num v          -> v
-        Var var        -> (env var)
+        Add exp1 exp2       -> (evalExp exp1 env) + (evalExp exp2 env)
+        Sub exp1 exp2       -> (evalExp exp1 env) - (evalExp exp2 env)        
+        Times exp1 exp2     -> (evalExp exp1 env) * (evalExp exp2 env)
+        Div exp1 exp2       -> (evalExp exp1 env) // (evalExp exp2 env)
+        Mod exp1 exp2       -> (evalExp exp1 env) % (evalExp exp2 env)
+        Exponential e1 e2   -> (evalExp e1 env) ^ (evalExp e2 env)
+        NthRootf exp1 exp2  -> floor ((toFloat (evalExp exp1 env)) ^ (1 / (toFloat (evalExp exp2 env))))
+        NthRootc exp1 exp2  -> ceiling ((toFloat (evalExp exp1 env)) ^ (1 / (toFloat (evalExp exp2 env))))
+        NthRootr exp1 exp2  -> round ((toFloat (evalExp exp1 env)) ^ (1 / (toFloat (evalExp exp2 env))))
+        Logf exp1 exp2      -> floor (logBase (toFloat (evalExp exp1 env)) (toFloat (evalExp exp2 env)))
+        Logc exp1 exp2      -> ceiling (logBase (toFloat (evalExp exp1 env)) (toFloat (evalExp exp2 env)))
+        Logr exp1 exp2      -> round (logBase (toFloat (evalExp exp1 env)) (toFloat (evalExp exp2 env)))
+        Num v               -> v
+        Var var             -> (env var)
 
 evalProg : Prog -> Env -> Env
 evalProg s env =
@@ -40,8 +62,14 @@ p1 = (Attr "ret" (Add (Num 11) (Num 9)))
 
 p2 : Prog
 p2 = Seq
-        (Attr "x" (Num 11))
-        (Attr "ret" (Add (Var "x") (Num 9)))
+        (Attr "x" (Sub (Times (Num 2) (Div (Num 20) (Num 6))) (Num 11)))
+        (Attr "ret" (Mod (Add (Var "x") (Num 9)) (Num 3)))
 
--- main = text (toString (lang p1))
-main = text (toString (lang p2))
+p3 = (Attr "ret" (NthRootf (Num 15) (Num 2)))
+p4 = (Attr "ret" (NthRootc (Num 15) (Num 2)))
+p5 = (Attr "ret" (Exponential (Num 2) (Num 5)))
+
+p6 = (Attr "ret" (Logf (Num 2) (Num 7)))
+p7 = (Attr "ret" (Logc (Num 2) (Num 7)))
+
+main = text (toString (lang p7))
